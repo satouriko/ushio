0cee6c1ce82187809839e2579ba3ec91f2fe6806
LI JIAHAO <lijiahao99131@gmail.com>
2018-10-11 19:15:11 +0800

    :sparkles: show subtitles

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,17 +2,35 @@ import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
 
-import UshioPlayer from '../../';
+import { UshioPlayer, UshioSubtitle } from '../../';
 
 import './style.css';
 
-class App extends Component<{}, {src: string}> {
+interface Video {src: string; subtitle: string; caption: string; }
+
+const videos: Video[] = [
+  {
+    src: 'https://tokimekiwakuwaku.rikako.moe/ときめきポポロン♪ .mp4',
+    subtitle: 'https://tokimekiwakuwaku.rikako.moe/ときめきポポロン♪  - subtitle.srt',
+    caption: 'https://tokimekiwakuwaku.rikako.moe/ときめきポポロン♪  - caption.srt',
+  },
+  {
+    src: 'https://tokimekiwakuwaku.rikako.moe/ひだまりデイズ-TV size ver.-.mp4',
+    subtitle: 'https://tokimekiwakuwaku.rikako.moe/ひだまりデイズ-TV size ver.- - subtitle.srt',
+    caption: 'https://tokimekiwakuwaku.rikako.moe/ひだまりデイズ-TV size ver.- - caption.srt',
+  },
+  {
+    src: 'https://tokimekiwakuwaku.rikako.moe/夜空はなんでも知ってるの.mp4',
+    subtitle: 'https://tokimekiwakuwaku.rikako.moe/夜空はなんでも知ってるの - subtitle.srt',
+    caption: 'https://tokimekiwakuwaku.rikako.moe/夜空はなんでも知ってるの - caption.srt',
+  },
+];
+
+class App extends Component<{}, Video> {
 
   constructor(props: {}) {
     super(props);
-    this.state = {
-      src: 'https://tokimekiwakuwaku.rikako.moe/ときめきポポロン♪ .mp4',
-    };
+    this.state = videos[0];
   }
 
   public render() {
@@ -21,18 +39,21 @@ class App extends Component<{}, {src: string}> {
         <h1>Ushio Player</h1>
         <h3>🍭 <ruby>
           汐<rp>（</rp><rt>Ushio</rt><rp>）</rp>
-        </ruby> is a lovely HTML5 video player with double-track SRT subtitle support.</h3>
+        </ruby> is a lovely HTML5 video player with multi-track SRT/VTT subtitle support.</h3>
         <UshioPlayer
           src={this.state.src}
           style={{
             width: '850px',
             height: '478px',
           }}
-        />
+        >
+          <UshioSubtitle type="srt" src={this.state.subtitle} />
+          <UshioSubtitle type="srt" src={this.state.caption} className="caption" />
+        </UshioPlayer>
         <p>
-          <button onClick={() => this.setState({src: 'https://tokimekiwakuwaku.rikako.moe/ときめきポポロン♪ .mp4'})}>ときめきポポロン♪ </button>
-          <button onClick={() => this.setState({src: 'https://tokimekiwakuwaku.rikako.moe/ひだまりデイズ-TV size ver.-.mp4'})}>ひだまりデイズ</button>
-          <button onClick={() => this.setState({src: 'https://tokimekiwakuwaku.rikako.moe/夜空はなんでも知ってるの.mp4'})}>夜空はなんでも知ってるの?</button>
+          <button onClick={() => this.setState(videos[0])}>ときめきポポロン♪ </button>
+          <button onClick={() => this.setState(videos[1])}>ひだまりデイズ</button>
+          <button onClick={() => this.setState(videos[2])}>夜空はなんでも知ってるの?</button>
         </p>
       </div>
     );
---
+++ src/components/subtitle.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { Component, CSSProperties } from 'react';
+import SubtitleUtils, { ISubtitle } from 'subtitle-utils';
+
+import '../stylesheets/subtitle.styl';
+
+export interface SubtitleProps {
+  src?: string;
+  value?: string;
+  type?: string;
+  style?: CSSProperties;
+  className?: string;
+}
+
+export interface SubtitlePropsInternal extends SubtitleProps {
+  currentTime?: number;
+}
+
+interface SubtitleStates {
+  src: string;
+  value: string;
+  subtitle: SubtitleUtils;
+  flyingSubtitles: ISubtitle[];
+}
+
+export class Subtitle extends Component<SubtitleProps, SubtitleStates> {
+
+  constructor(props: SubtitlePropsInternal) {
+    super(props);
+    this.state = {
+      src: props.src,
+      value: props.value,
+      subtitle: null,
+      flyingSubtitles: [],
+    };
+  }
+
+  public async componentDidMount() {
+    const subtitle = await Subtitle.initSubtitle(this.props);
+    this.setState({ subtitle });
+  }
+
+  public async componentDidUpdate(prevProps: SubtitlePropsInternal, prevState: SubtitleStates) {
+    if (this.state.subtitle === null) {
+      const subtitle = await Subtitle.initSubtitle(this.props);
+      this.setState({ subtitle });
+    }
+  }
+
+  private static async initSubtitle(props: SubtitlePropsInternal): Promise<SubtitleUtils> {
+    let text = '';
+    if (props.src) {
+      const resp = await fetch(props.src);
+      text = await resp.text();
+    } else {
+      text = props.value;
+    }
+    switch (props.type.toLowerCase()) {
+      case 'srt':
+        return await SubtitleUtils.fromSRT(text);
+      default:
+        return await SubtitleUtils.fromVTT(text);
+    }
+  }
+
+  public static getDerivedStateFromProps(nextProps: SubtitlePropsInternal, prevState: SubtitleStates) {
+    if (nextProps.src !== prevState.src || nextProps.value !== prevState.value) {
+      return { src: nextProps.src, value: nextProps.value, subtitle: null as SubtitleUtils };
+    }
+    if (!prevState.subtitle) return null;
+    const flyingSubtitles: ISubtitle[] = [];
+    prevState.subtitle.subtitles.forEach(subtitle => {
+      const currentTime = nextProps.currentTime * 1000;
+      if (currentTime > subtitle.startTime && currentTime < subtitle.endTime) {
+        flyingSubtitles.push(subtitle);
+      }
+    });
+    return {flyingSubtitles};
+  }
+
+  public render() {
+    return (
+      <div className={`ushio-player-subtitle-wrap ${this.props.className || ''}`} style={this.props.style}>
+        {
+          this.state.flyingSubtitles.map(
+            subtitle => subtitle.texts.map(
+              (text, index) => (<span className="ushio-player-subtitle" key={index} dangerouslySetInnerHTML={{__html: text}} />)
+              )
+          )
+        }
+      </div>
+    );
+  }
+
+}
