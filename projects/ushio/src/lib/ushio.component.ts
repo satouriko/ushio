f5c1060927f99defb4a23be0415f9251a1db3d62
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-10-02 23:48:49 +0800

    :sparkles: slider

--- projects/ushio/src/lib/ushio.component.ts
+++ projects/ushio/src/lib/ushio.component.ts
@@ -9,7 +9,7 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { Observable, of, merge, Subject, fromEvent, timer, Subscription, EMPTY } from 'rxjs';
-import { distinctUntilChanged, map, mapTo, switchAll } from 'rxjs/operators';
+import { concatMap, distinctUntilChanged, map, mapTo, switchAll, takeUntil } from 'rxjs/operators';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Directive({
@@ -38,6 +38,7 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
   @Input() muted;
 
   @ViewChild('video', {static: true}) video;
+  @ViewChild('sliderTrack', {static: true}) sliderTrack;
 
   @ContentChildren(UshioSubtitles) subtitles!: QueryList<UshioSubtitles>;
   private subtitles$: Observable<string[]>;
@@ -46,6 +47,7 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
   private sub: Subscription[] = [];
 
   private mouseMove$ = fromEvent(document, 'mousemove');
+  private mouseUp$ = fromEvent(document, 'mouseup');
   private activeState$ = this.mouseMove$.pipe(
     map((e: MouseEvent) => {
       const rect = this.video.nativeElement.getBoundingClientRect();
@@ -54,37 +56,38 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
         e.clientY > rect.top &&
         e.clientY < rect.bottom
           ? merge(
-              of({ hover: true, noCursor: false }),
+              of(true),
               e.clientY < rect.bottom - 46 ? timer(3000).pipe(
-                mapTo({ hover: false, noCursor: true })
+                mapTo(false)
               ) : EMPTY
             )
-          : of({ hover: false, noCursor: false });
+          : of(false);
     }),
     switchAll(),
-    distinctUntilChanged((a, b) => {
-      return a.hover === b.hover && a.noCursor === b.noCursor;
-    })
+    distinctUntilChanged()
   );
   private hover = false;
-  private noCursor = false;
+  private mouseDown = false;
   get hoverClass(): string {
-    return this.hover ? 'mouse-hover' : '';
+    return (this.hover || this.mouseDown) ? ' mouse-hover' : '';
   }
   get noCursorClass(): string {
-    return this.noCursor ? 'no-cursor' : '';
+    return !(this.hover || this.mouseDown) ? ' no-cursor' : '';
+  }
+  get mouseDownClass(): string {
+    return this.mouseDown ? ' mouse-down' : '';
   }
   get pausedClass(): string {
-    return this.video.nativeElement.paused ? 'video-state-pause' : 'video-state-play';
+    return this.video.nativeElement.paused ? ' video-state-pause' : ' video-state-play';
   }
   private currentTime = 0;
   private duration = 0;
   private bufferedTime = 0;
   get currentTimeStr(): string {
-    return this.formatDuration(this.video.nativeElement.currentTime);
+    return this.formatDuration(this.currentTime);
   }
   get durationStr(): string {
-    return this.formatDuration(this.video.nativeElement.duration);
+    return this.formatDuration(this.duration);
   }
   get bufferedProgress(): SafeStyle {
     return this.sanitization.bypassSecurityTrustStyle(
@@ -96,6 +99,13 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
       `transform: scaleX(${this.currentTime / this.duration})`
     );
   }
+  get thumbPosition(): SafeStyle {
+    return this.sanitization.bypassSecurityTrustStyle(
+      `left: ${this.currentTime / this.duration * 100}%`
+    );
+  }
+
+  private timeUpdate: Subscription;
 
   constructor(private sanitization: DomSanitizer) { }
 
@@ -125,13 +135,12 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
 
   ngAfterViewInit() {
     this.sub.push(this.activeState$.subscribe(state => {
-      this.hover = state.hover;
-      this.noCursor = state.noCursor;
+      this.hover = state;
     }));
-    this.sub.push(fromEvent(this.video.nativeElement, 'timeupdate').subscribe(e => {
+    this.timeUpdate = fromEvent(this.video.nativeElement, 'timeupdate').subscribe(() => {
       this.currentTime = this.video.nativeElement.currentTime;
-    }));
-    this.sub.push(fromEvent(this.video.nativeElement, 'progress').subscribe(e => {
+    });
+    this.sub.push(fromEvent(this.video.nativeElement, 'progress').subscribe(() => {
       this.bufferedTime = ((timeRanges, currentTime) => {
         const length = timeRanges.length;
         for (let i = 0; i < length; i++) {
@@ -146,12 +155,49 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
         return currentTime;
       })(this.video.nativeElement.buffered, this.video.nativeElement.currentTime);
     }));
-    this.sub.push(fromEvent(this.video.nativeElement, 'loadedmetadata').subscribe(e => {
+    this.sub.push(fromEvent(this.video.nativeElement, 'loadedmetadata').subscribe(() => {
       this.duration = this.video.nativeElement.duration;
+    }));
+    const thumbMouseDown$ = fromEvent(this.sliderTrack.nativeElement, 'mousedown');
+    const thumbDrag$ = thumbMouseDown$.pipe(
+      concatMap(() => {
+        return this.mouseMove$.pipe(
+          takeUntil(this.mouseUp$),
+          map((moveEvent: MouseEvent) => {
+            const rect = this.sliderTrack.nativeElement.getBoundingClientRect();
+            let progress = moveEvent.x - rect.left;
+            if (progress < 0) {
+              progress = 0;
+            } else if (progress > rect.width) {
+              progress = rect.width;
+            }
+            return progress / rect.width;
+          })
+        );
+      })
+    );
+    this.sub.push(thumbMouseDown$.subscribe((e: MouseEvent) => {
+      this.mouseDown = true;
+      this.timeUpdate.unsubscribe();
+      const rect = (e.currentTarget as HTMLDivElement).getBoundingClientRect();
+      this.currentTime = (e.clientX - rect.left) / rect.width * this.duration;
+    }));
+    this.sub.push(thumbDrag$.subscribe(e => {
+      this.currentTime = e * this.duration;
+    }));
+    this.sub.push(this.mouseUp$.subscribe(() => {
+      if (this.mouseDown) {
+        this.video.nativeElement.currentTime = this.currentTime;
+        this.timeUpdate = fromEvent(this.video.nativeElement, 'timeupdate').subscribe(() => {
+          this.currentTime = this.video.nativeElement.currentTime;
+        });
+        this.mouseDown = false;
+      }
     }));
   }
 
   ngOnDestroy() {
+    this.timeUpdate.unsubscribe();
     this.sub.forEach(sub => sub.unsubscribe());
   }
 
@@ -167,11 +213,6 @@ export class UshioComponent implements OnInit, AfterContentInit, AfterViewInit, 
     } else {
       this.video.nativeElement.pause();
     }
-  }
-
-  changeCurrentTime(e: MouseEvent) {
-    const rect = (e.currentTarget as HTMLDivElement).getBoundingClientRect();
-    this.video.nativeElement.currentTime = (e.clientX - rect.left) / rect.width * this.duration;
   }
 
   private formatDuration = (duration: number): string => {
